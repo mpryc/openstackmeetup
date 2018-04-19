46c2994946133469365d2b0e61657ad1d15f30fb
Arek Sz <arekxd@gmail.com>
2018-04-19 17:55:07 +0000

    Add python2 compatibility.
    Change-Id: I3d0ff4c548a1dc0f3b364904969d8e1de25ed25f

--- openstackmeetup/konkurs1.py
+++ openstackmeetup/konkurs1.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+from __future__ import print_function
 
 TERM = '576974616d79206e6120392073706f746b616e69752057726f636c6177204f70656e537461636b204d656574757021'
 
-print(TERM.decode('hex'))
+def decode_secret_string(term):
+   return term.decode('hex')
 
+print(decode_secret_string(TERM))
